943e1a0ec74b039c0929e86eb8d3f3e27f18ca55
陳卓妍 <D1249326@o365.fcu.edu.tw>
2025-12-05 10:28:35 +0800

    feat: add

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     printf("=== C Language Calculator ===\n");
 
     // Operations placeholders
     printf("System Check OK.\n");
+
+    int result = add(5, 3);
+    printf("Addition Result: %d\n", result);
     return 0;
 }
